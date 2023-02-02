2868217296fe1392524d95d8ff12bada111fe7f1
fann <f.affnd@gmail.com>
2023-02-02 20:30:12 +0700

    Initial commit

---
+++ app/src/main/java/edu/uksw/fti/pam/pam_activity/HomeActivity.kt
@@ -0,0 +1,44 @@
+package edu.uksw.fti.pam.pam_activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import edu.uksw.fti.pam.pam_activity.ui.theme.PAM_ActivityTheme
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PAM_ActivityTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    val username = getIntent().getStringExtra("username") ?: ""
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    PAM_ActivityTheme {
+        Greeting("Android")
+    }
+}

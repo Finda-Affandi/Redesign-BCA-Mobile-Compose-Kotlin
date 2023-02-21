406679a6e59ddfddcd9cc81b613bf230a6e146d6
fann <f.affnd@gmail.com>
2023-02-21 15:05:12 +0700

    yeah

--- app/src/main/java/edu/uksw/fti/pam/pam_activity/MainActivity.kt
+++ app/src/main/java/edu/uksw/fti/pam/pam_activity/MainActivity.kt
@@ -1,22 +1,12 @@
 package edu.uksw.fti.pam.pam_activity
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import edu.uksw.fti.pam.pam_activity.contracts.SignupContract
 import edu.uksw.fti.pam.pam_activity.ui.screens.LoginForm
 import edu.uksw.fti.pam.pam_activity.ui.theme.PAM_ActivityTheme
 
--- app/src/main/java/edu/uksw/fti/pam/pam_activity/SignUpActivity.kt
+++ app/src/main/java/edu/uksw/fti/pam/pam_activity/SignUpActivity.kt
@@ -3,23 +3,12 @@ package edu.uksw.fti.pam.pam_activity
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import android.service.autofill.OnClickAction
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.horizontalScroll
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import edu.uksw.fti.pam.pam_activity.ui.screens.SignUp
 import edu.uksw.fti.pam.pam_activity.ui.theme.PAM_ActivityTheme
 
